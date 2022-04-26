45b39961465454757c91f0417b18f97fcbf6e84b
algins <igors.algins@gmail.com>
2022-04-26 10:07:32 +0300

    Fix action format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1>, <filepath2>')
-  .action((filePath1, filePath2, format) => {
+  .action((filePath1, filePath2, { format }) => {
     const diff = genDiff(filePath1, filePath2, format);
     console.log(diff);
   });
